d6522751a70d5181850f89bfdba255de50d444f8
Eugene Vedensky <Work@Genyas-Air.lan>
2018-12-10 20:17:15 -0500

    final push before full stack app

--- src/app/poke-entry/poke-sprite/poke-sprite.component.ts
+++ src/app/poke-entry/poke-sprite/poke-sprite.component.ts
@@ -11,8 +11,24 @@ export class PokeSpriteComponent implements OnInit {
   constructor(private pokeService: PokeService) {
     this.pokeService.pokePayloadChange.subscribe(({ spriteImageEndpoint }) => {
       this.spriteImageEndpoint = spriteImageEndpoint;
+      this.addBlurEffectOnLoad().then(() => {
+        document
+          .querySelector("#sprite-image")
+          .classList.remove("blurin-animation-active");
+      });
     });
   }
 
   ngOnInit() {}
+
+  addBlurEffectOnLoad() {
+    return new Promise((res, rej) => {
+      document
+        .querySelector("#sprite-image")
+        .classList.add("blurin-animation-active");
+      setTimeout(() => {
+        res();
+      }, 500);
+    });
+  }
 }
